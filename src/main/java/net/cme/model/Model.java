00f2c78d689f58aa9e3b86e51874a538cd879371
Anten Skrabec <cebarks@gmail.com>
2014-07-24 01:05:00 -0600

    Basic OBJ parser for bunny.obj

--- src/main/java/net/cme/model/Model.java
+++ src/main/java/net/cme/model/Model.java
@@ -1,27 +1,104 @@
 package net.cme.model;
 
-import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.opengl.GL15.*;
+import static org.lwjgl.opengl.GL11.GL_FLOAT;
+import static org.lwjgl.opengl.GL11.GL_TRIANGLES;
+import static org.lwjgl.opengl.GL11.GL_UNSIGNED_INT;
+import static org.lwjgl.opengl.GL11.glDrawElements;
+import static org.lwjgl.opengl.GL15.GL_ARRAY_BUFFER;
+import static org.lwjgl.opengl.GL15.GL_ELEMENT_ARRAY_BUFFER;
+import static org.lwjgl.opengl.GL15.GL_STATIC_DRAW;
+import static org.lwjgl.opengl.GL15.glBindBuffer;
+import static org.lwjgl.opengl.GL15.glBufferData;
+import static org.lwjgl.opengl.GL15.glGenBuffers;
+import static org.lwjgl.opengl.GL20.glDisableVertexAttribArray;
+import static org.lwjgl.opengl.GL20.glEnableVertexAttribArray;
+import static org.lwjgl.opengl.GL20.glVertexAttribPointer;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+
+import net.cme.engine.CMEngine;
+import net.cme.util.Util;
+import net.cme.util.Vector3;
 
 public class Model {
-	private List<Vertex> unbakedVertices = new ArrayList<Vertex>();
+	private List<Vertex> vertices = new ArrayList<Vertex>();
+	private List<Integer> indicies = new ArrayList<Integer>();
+
 	private int vbo;
+	private int ibo;
 	private int size;
-	
+
 	public Model(String location) {
-		//Load vertices here
+		try {
+			parseObj(new FileInputStream(new File("src/main/resources/" + location)));
+		} catch (FileNotFoundException e) {
+			CMEngine.LOGGER.error("Couldn't load model: " + location, e);
+		}
+
 		vbo = glGenBuffers();
-		//for blah blah blah create vao
+		ibo = glGenBuffers();
+		size = indicies.size();
+
+		glBindBuffer(GL_ARRAY_BUFFER, vbo);
+		glBufferData(GL_ARRAY_BUFFER, Util.vertexToBuffer(vertices.toArray(new Vertex[] {})), GL_STATIC_DRAW);
+
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, Util.intToBuffer(Util.integerListToIntArray(indicies)), GL_STATIC_DRAW);
 	}
-	
+
 	public void render() {
-		
+		glEnableVertexAttribArray(0);
+
+		glBindBuffer(GL_ARRAY_BUFFER, vbo);
+		glVertexAttribPointer(0, Vertex.SIZE, GL_FLOAT, false, Vertex.SIZE * 4, 0);
+
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+		glDrawElements(GL_TRIANGLES, size, GL_UNSIGNED_INT, 0);
+
+		glDisableVertexAttribArray(0);
 	}
-	
-	public void addVertex(Vertex vertex) {
-		unbakedVertices.add(vertex);
+
+	public void parseObj(InputStream stream) {
+		try {
+			Scanner scan = new Scanner(stream);
+
+			String line;
+			while ((line = scan.nextLine()) != null) {
+				if (line.startsWith("#"))
+					continue;
+
+				if (line.startsWith("v ")) {
+					String[] raw = line.substring(2).split(" ");
+					Vertex vertex = new Vertex(new Vector3(Float.valueOf(raw[0]), Float.valueOf(raw[1]), Float.valueOf(raw[2])));
+					addVertex(vertex);
+					System.out.printf("v %f %f %f\n", vertex.position.x, vertex.position.y, vertex.position.z);
+				}
+
+				if (line.startsWith("f ")) {
+					String[] raw = line.substring(2).split(" ");
+					for (String s : raw) {
+						addIndex(Integer.parseInt(s));
+					}
+					System.out.printf("f %s %s %s\n", raw[0], raw[1], raw[2]);
+				}
+			}
+			scan.close();
+		} catch (Exception e) {
+			// CMEngine.LOGGER.error("Error parsing OBJ file.", e);
+		}
+	}
+
+	private void addVertex(Vertex vertex) {
+		vertices.add(vertex);
+	}
+
+	private void addIndex(int parseInt) {
+		indicies.add(parseInt);
 	}
 }
---
+++ src/main/java/net/cme/util/Util.java
@@ -0,0 +1,49 @@
+package net.cme.util;
+
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
+import java.util.List;
+
+import org.lwjgl.BufferUtils;
+
+import net.cme.model.Vertex;
+
+public class Util {
+	public static FloatBuffer vertexToBuffer(Vertex[] vertex) {
+		FloatBuffer buffer = BufferUtils.createFloatBuffer(vertex.length * Vertex.SIZE);
+		for (Vertex v : vertex) {
+			buffer.put(v.position.x);
+			buffer.put(v.position.y);
+			buffer.put(v.position.z);
+		}
+		
+		buffer.flip();
+		
+		return buffer;
+	}
+
+	public static int[] integerListToIntArray(List<Integer> intList) {
+		int[] arr = new int[intList.size()];
+
+		int c = 0;
+		for (Integer i : intList) {
+			arr[c] = i;
+			c++;
+		}
+
+		return arr;
+	}
+
+	public static IntBuffer intToBuffer(int[] ints) {
+		IntBuffer buffer = BufferUtils.createIntBuffer(ints.length);
+		
+		for (int i : ints) {
+			buffer.put(i);
+		}
+		
+		buffer.flip();
+
+		return buffer;
+	}
+
+}

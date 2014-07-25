6e9bd2334d37a43fffc5c29b33ffa22d248ff45d
Anten Skrabec <cebarks@gmail.com>
2014-07-25 14:07:15 -0600

    Missed Shader.java

--- src/main/java/net/cme/model/Shader.java
+++ src/main/java/net/cme/model/Shader.java
@@ -1,5 +1,6 @@
 package net.cme.model;
 
+import static org.lwjgl.opengl.GL11.GL_FALSE;
 import static org.lwjgl.opengl.GL20.GL_COMPILE_STATUS;
 import static org.lwjgl.opengl.GL20.GL_LINK_STATUS;
 import static org.lwjgl.opengl.GL20.GL_VALIDATE_STATUS;
@@ -7,11 +8,8 @@ import static org.lwjgl.opengl.GL20.glAttachShader;
 import static org.lwjgl.opengl.GL20.glCompileShader;
 import static org.lwjgl.opengl.GL20.glCreateProgram;
 import static org.lwjgl.opengl.GL20.glCreateShader;
-<<<<<<< HEAD:src/main/java/net/cme/model/Shader.java
 import static org.lwjgl.opengl.GL20.glGetProgramInfoLog;
 import static org.lwjgl.opengl.GL20.glGetProgrami;
-=======
->>>>>>> 2578798d5d2f4aaa983099f0898a655351286083:src/main/java/net/cme/util/Shader.java
 import static org.lwjgl.opengl.GL20.glGetShaderInfoLog;
 import static org.lwjgl.opengl.GL20.glGetShaderi;
 import static org.lwjgl.opengl.GL20.glLinkProgram;
@@ -31,16 +29,13 @@ public class Shader {
 
 	public Shader() {
 		program = glCreateProgram();
-<<<<<<< HEAD:src/main/java/net/cme/model/Shader.java
-=======
 
->>>>>>> 2578798d5d2f4aaa983099f0898a655351286083:src/main/java/net/cme/util/Shader.java
 		if (program == 0) {
 			CMEngine.LOGGER.error("Program could not be created: " + glGetShaderInfoLog(program, 1024));
 			CMEngine.exitOnError(1, new Exception());
 		}
 	}
-	
+
 	public void bind() {
 		glUseProgram(program);
 	}
@@ -67,28 +62,24 @@ public class Shader {
 
 	public void compileShader() {
 		glLinkProgram(program);
-<<<<<<< HEAD:src/main/java/net/cme/model/Shader.java
-		
+
 		if (glGetProgrami(program, GL_LINK_STATUS) == 0) {
 			CMEngine.LOGGER.error("Could not link program: \n" + glGetProgramInfoLog(program, 1024));
-=======
+		}
 
 		if (glGetShaderi(program, GL_LINK_STATUS) == GL_FALSE) {
 			CMEngine.LOGGER.error("Could not link program: \n" + glGetShaderInfoLog(program, 1024));
->>>>>>> 2578798d5d2f4aaa983099f0898a655351286083:src/main/java/net/cme/util/Shader.java
 			CMEngine.exitOnError(1, new LWJGLException());
 		}
 
 		glValidateProgram(program);
-<<<<<<< HEAD:src/main/java/net/cme/model/Shader.java
-		
+
 		if (glGetProgrami(program, GL_VALIDATE_STATUS) == 0) {
 			CMEngine.LOGGER.error("Could not validate program: \n" + glGetProgramInfoLog(program, 1024));
-=======
+		}
 
 		if (glGetShaderi(program, GL_VALIDATE_STATUS) == GL_FALSE) {
 			CMEngine.LOGGER.error("Could not validate program: \n" + glGetShaderInfoLog(program, 1024));
->>>>>>> 2578798d5d2f4aaa983099f0898a655351286083:src/main/java/net/cme/util/Shader.java
 			CMEngine.exitOnError(1, new LWJGLException());
 		}
 	}
@@ -96,7 +87,7 @@ public class Shader {
 	public String loadShaderSource(String location) {
 		StringBuilder shaderSource = new StringBuilder();
 		BufferedReader shaderReader = null;
-		
+
 		try {
 			shaderReader = new BufferedReader(new FileReader("src/main/resources/" + location));
 			String line;
